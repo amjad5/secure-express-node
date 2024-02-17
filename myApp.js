41a4ca3a1b10cb611709d8ef8ef7ea8d284631f8
Amjad h <amjad.hussain544@gmail.com>
2024-02-17 11:45:22 +0000

    enable helmet middleware with single config

--- myApp.js
+++ myApp.js
@@ -1,36 +1,56 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-const bcrypt = require('bcrypt');
 
-commented to use the helmet's middleware, passing single configuration object
-app.use(helmet.hidePoweredBy())
+// commented to use the helmet's middleware, passing single configuration object
+// app.use(helmet.hidePoweredBy())
 
-// disable webpage to be rendered in external frames 
-app.use(helmet.frameguard({action: 'deny'}))
+// // disable webpage to be rendered in external frames 
+// app.use(helmet.frameguard({action: 'deny'}))
 
-app.use(helmet.xssFilter())
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
+// app.use(helmet.xssFilter())
+// app.use(helmet.noSniff())
+// app.use(helmet.ieNoOpen())
 
 
 ninetyDaysInSeconds = 90*24*60*60;
 hstsConfig = {maxAge: ninetyDaysInSeconds, force: true}
 // app.use(helmet.hsts(hstsConfig))
 
-// dns prefatch controll 
-app.use(helmet.dnsPrefetchControl())
+// // dns prefatch controll 
+// app.use(helmet.dnsPrefetchControl())
 
-// disable client cache, comment if client cache is required
-app.use(helmet.noCache())
+// // disable client cache, comment if client cache is required
+// app.use(helmet.noCache())
 
-// list of allowed sources to load scripts, stylesheets, fonts, frames, media,
-directives = {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}
+// // list of allowed sources to load scripts, stylesheets, fonts, frames, media,
+// //scriptSrc: ["'self'", 'gitpod.io']}
+// directives = {defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com']}
+// // let scriptSrc = {}
+// // set Content security policy
+// // protect app from XSS vulnerabilities, undesired tracking, malicious frames etc.
+// app.use(helmet.contentSecurityPolicy({directives: directives}))
 
-// set Content security policy
-// protect app from XSS vulnerabilities, undesired tracking, malicious frames etc.
-app.use(helmet.contentSecurityPolicy({directives: directives}))
 
+
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+    }
+  },
+  dnsPrefetchControl: false,     // disable
+  hsts: hstsConfig,
+  xXssProtection: true,
+  noSniff: true,
+  ieNoOpen: true,
+  hidePoweredBy: true
+
+}))
 
 
 
